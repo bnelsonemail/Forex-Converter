1d139cb4462f331b70d6b3088e070d73a3ae5f77
bnelson <bnelsonemail@icloud.com>
2024-09-02 03:26:20 -0400

    revisions to app.py to work on conversion route

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_debugtoolbar import DebugToolbarExtension
 from config import logger, DevelopmentConfig # or ProductionConfig, TestingConfig
 from dotenv import load_dotenv, find_dotenv
 #from forex_converter import forex_BTC
+import requests
 import os
 
 
@@ -35,10 +36,44 @@ def convert():
     return render_template('convert.html')
 
 
-@app.route('/conversion')
+@app.route('/conversion', methods=['GET', 'POST'])
 def conversion():
-    """Calls the API and returns values.  Redirects to convert.html"""
-    return redirect('/convert')
+    """Calls the API and returns values. Redirects to convert.html"""
+
+    
+        
+    
+    # Get the access key from the .env file
+    access_key = os.getenv('ACCESS_KEY')
+
+    # Define the URL and parameters
+    url = 'https://api.exchangerate.host/live'
+    params = {
+        'access_key': access_key
+    }
+
+    # Make the GET request to the API
+    response = requests.get(url, params=params)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Parse the JSON response
+        data = response.json()
+        
+        # Process the data from user
+        # Retrieve values from home.html form
+        from_currency = request.form.get('from_curr') 
+        to_currency = request.form.get('to_curr')
+        amount = request.form.get('amount') 
+    
+        # TODO: perform calculation from forex_converter.py script
+        
+        
+        # redirect to convert.html
+        return render_template('convert.html', data=data)
+    else:
+        # Handle the error
+        return f"Error: {response.status_code}"
 
 
 
