d456bf9da893f4e36965124dd9e14b469ac7f688
bnelson <bnelsonemail@icloud.com>
2024-09-02 02:55:35 -0400

    revisions in routes in app.py, added images to be included on pages, imported forex and updated requirements.txt, revisions to html templates and styling with Tailwind, updated .gitignore to exclude items from GH

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template, request
-# from flask_debugtoolbar import DebugToolbarExtension
+from flask import Flask, render_template, request, json, jsonify, redirect
+from flask_debugtoolbar import DebugToolbarExtension
 from config import logger, DevelopmentConfig # or ProductionConfig, TestingConfig
 from dotenv import load_dotenv, find_dotenv
-# from forex import forex_BTC
+#from forex_converter import forex_BTC
 import os
 
 
@@ -15,13 +15,30 @@ app = Flask(__name__)
 # Call config files
 app.config.from_object(DevelopmentConfig)
 # app.config['DEBUG'] = 'DEBUG_TB_INTERCEPT_REDIRECTS = True'
-# debug = DebugToolbarExtension(app)
+debug = DebugToolbarExtension(app)
 
 @app.route('/') # Home Page
 def home():
+    """Renders Home Page with a form for user input to convert currency rates"""
     return render_template ('home.html')
 
 
+@app.route('/table')
+def table():
+    """Renders table.html which provides a link for user to look up currency codes"""
+    return render_template('table.html')
+
+
+@app.route('/convert')
+def convert():
+    """Renders Page to display the converted currency requested by user"""
+    return render_template('convert.html')
+
+
+@app.route('/conversion')
+def conversion():
+    """Calls the API and returns values.  Redirects to convert.html"""
+    return redirect('/convert')
 
 
 
---
+++ forex_converter.py
@@ -0,0 +1,5 @@
+from forex_python.converter import CurrencyCodes, CurrencyRates 
+from datetime import date, datetime # import the date and datetime modules from datetime library
+
+
+
